04a6f8fccc2ce45bd5388a2fbd7bbbfde658e624
vivek <inavallivivek6@gmail.com>
2026-01-25 11:07:44 +0530

    spl_metadata added

--- ts/cluster1/spl_metadata.ts
+++ ts/cluster1/spl_metadata.ts
@@ -2,7 +2,7 @@ import fs from "fs";
 
 const wallet = JSON.parse(
     fs.readFileSync(
-        "",
+        "wallet_path",
         "utf8"
     )
 )
@@ -11,45 +11,64 @@ import {
     createMetadataAccountV3, 
     CreateMetadataAccountV3InstructionAccounts, 
     CreateMetadataAccountV3InstructionArgs,
-    DataV2Args
+    DataV2Args,
+    findMetadataPda
 } from "@metaplex-foundation/mpl-token-metadata";
-import { createSignerFromKeypair, signerIdentity, publicKey } from "@metaplex-foundation/umi";
+import { createSignerFromKeypair, signerIdentity, publicKey, none } from "@metaplex-foundation/umi";
 import { bs58 } from "@coral-xyz/anchor/dist/cjs/utils/bytes";
+import { PublicKey } from "@solana/web3.js";
 
 // Define our Mint address
-const mint = publicKey("3seuNRFgbkEwcAFLgbSuikaAUom1tr6EXY4MiK4MqyG3")
+const mint = publicKey("6YonGwmWLd3fVd1uxSgDQp3oauM8cdnMHuBfjTKoR2ry")
 
 // Create a UMI connection
 const umi = createUmi('https://api.devnet.solana.com');
 const keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
 const signer = createSignerFromKeypair(umi, keypair);
-umi.use(signerIdentity(createSignerFromKeypair(umi, keypair)));
+umi.use(signerIdentity(signer))
+
+const metadata = findMetadataPda(umi, { mint });
+
 
 (async () => {
     try {
         // Start here
-        // let accounts: CreateMetadataAccountV3InstructionAccounts = {
-        //     ???
-        // }
+        let accounts: CreateMetadataAccountV3InstructionAccounts = { 
+            metadata,
+            mint,
+            mintAuthority: signer,
+            payer: signer,
+            updateAuthority: keypair.publicKey,
+        }
 
-        // let data: DataV2Args = {
-        //     ???
-        // }
+        let data: DataV2Args = {
+            name: "VIVEK",
+            symbol: "🦀",
+            uri: "https://arweave.net/AbC123xyz",
+            sellerFeeBasisPoints: 1000,
+            creators: none(),
+            collection: none(),
+            uses: none()
+            
+        }
 
-        // let args: CreateMetadataAccountV3InstructionArgs = {
-        //     ???
-        // }
+        let args: CreateMetadataAccountV3InstructionArgs = {
+            data,
+            isMutable: false,
+            collectionDetails: none()
+        }
 
-        // let tx = createMetadataAccountV3(
-        //     umi,
-        //     {
-        //         ...accounts,
-        //         ...args
-        //     }
-        // )
+         let tx = createMetadataAccountV3(
+            umi,
+            {
+                ...accounts,
+                ...args
+            }
+        )
 
-        // let result = await tx.sendAndConfirm(umi);
-        // console.log(bs58.encode(result.signature));
+
+        let result = await tx.sendAndConfirm(umi);
+        console.log(bs58.encode(result.signature));
     } catch(e) {
         console.error(`Oops, something went wrong: ${e}`)
     }
--- ts/cluster1/spl_mint.ts
+++ ts/cluster1/spl_mint.ts
@@ -4,7 +4,7 @@ import fs from "fs";
 
 const wallet = JSON.parse(
     fs.readFileSync(
-        "",
+        "/Users/vivekinavalli/.config/solana/id.json",
         "utf8"
     )
 )
